818e8d5c82a4e285d6fb43271ffa259e6f57fd85
iyazmultani007 <iyaz.multani007@gmail.com>
2025-10-10 10:26:44 +0100

    resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -85,7 +85,7 @@ function resume() {
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
               <Summary feedback={feedback} />
-              <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+              <ATS score={feedback?.ATS?.score || 0} suggestions={feedback?.ATS?.tips || []} />
               <Details feedback={feedback} />
             </div>
           ) : (
